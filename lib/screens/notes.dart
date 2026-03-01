dd9c9a06c6ba13858850be9bece08dede17b9237
Anıl Murat Hırça <anilmurathirca@gmail.com>
2026-03-01 15:15:42 +0300

    feat: initialize stateful screens for dices, notes and spells

---
+++ lib/screens/notes.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Notes_Screen
+    extends StatefulWidget {
+  const Notes_Screen({super.key});
+
+  @override
+  State<Notes_Screen> createState() =>
+      _Notes_ScreenState();
+}
+
+class _Notes_ScreenState
+    extends State<Notes_Screen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/screens/spells.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Spells_Screen
+    extends StatefulWidget {
+  const Spells_Screen({super.key});
+
+  @override
+  State<Spells_Screen> createState() =>
+      _Spells_ScreenState();
+}
+
+class _Spells_ScreenState
+    extends State<Spells_Screen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
